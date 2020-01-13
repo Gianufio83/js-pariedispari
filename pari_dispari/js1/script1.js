f7a49602302123b18edfdd1e3e5498a07717fb13
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-13 23:14:40 +0100

    esercizio pari e dispari rivisto e completo

--- pari_dispari/js1/script1.js
+++ pari_dispari/js1/script1.js
@@ -8,17 +8,31 @@
 var scelta = prompt('Scegli pari o dispari');
 console.log(scelta);
 var numero = parseInt(prompt('Scegli un numero tra 1 e 5'));
+while (isNan(numero)) {
+  var numero = parseInt(prompt('Devi inserire un numero'));
+}
 console.log(numero);
 var numeroCasuale = getRandomIntInclusive(1,5);
 console.log(numeroCasuale);
 var somma = numeroCasuale + numero;
 console.log(somma);
-if (somma % 0 == scelta) {
-  console.log('Hai vinto');
+
+if (somma % 0 == 0) {
+  var pari = true;
 }
 else {
+  var pari = false;
+}
+if (scelta == 'pari' && pari == true) {
+  console.log('Hai vinto');
+} else if (scelta == 'dispari' && pari == false) {
+  console.log('Hai vinto');
+} else {
   console.log('Hai perso');
 }
+
+
+
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
